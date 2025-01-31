3fc85021ba2435133af3a2adf7af9e0f8c1231cf
biplab-mistry <kr.biplabmistry@gmail.com>
2025-01-31 10:35:45 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primary:'#111111',
+                secondary:'#179800',
+                accent:'#179800'
+            },
+        }
+    }
+}
